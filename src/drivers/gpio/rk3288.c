f93ff4bdb86873eac6b9f9c85d7aef31142d87eb
zyw <zyw@rock-chips.com>
2014-05-16 17:03:20 +0800

    rksdk: support rk3288 gpio
    Change-Id: I3d78a8c1820a43162a062d68f27810dc794b132f

---
+++ src/drivers/gpio/rk3288.c
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <assert.h>
+#include <libpayload.h>
+#include <stdint.h>
+
+#include "base/container_of.h"
+#include "drivers/gpio/rk3288.h"
+#include "drivers/gpio/gpio.h"
+
+static int rk_gpio_bank_reg[GPIO_BANK_NUM] = {
+	0xFF750000,   //gpio0
+	0xFF780000,   //gpio1
+	0xFF790000,   //gpio2
+	0xFF7A0000,   //gpio3
+	0xFF7B0000,   //gpio4
+	0xFF7C0000,   //gpio5
+	0xFF7D0000,   //gpio6
+	0xFF7E0000,   //gpio7
+	0xFF7F0000,   //gpio8
+};
+
+static inline void rk_gpio_bit_op(int *regbase, unsigned int offset, unsigned int bit, unsigned char flag)
+{
+	unsigned int val = readl(regbase + offset);
+
+	if (flag)
+		val |= bit;
+	else
+		val &= ~bit;
+
+	writel(val, regbase + offset);
+}
+
+static inline void rk_gpio_set_pin_level(int *regbase, unsigned int bit, eGPIOPinLevel_t level)
+{
+	rk_gpio_bit_op(regbase, GPIO_SWPORT_DR, bit, level);
+}
+
+static inline int rk_gpio_get_pin_level(int *regbase, unsigned int bit)
+{
+	return ((readl(regbase + GPIO_EXT_PORT) & bit) != 0);
+}
+
+static inline void rk_gpio_set_pin_direction(int *regbase, unsigned int bit, eGPIOPinDirection_t direction)
+{
+	rk_gpio_bit_op(regbase, GPIO_SWPORT_DDR, bit, direction);
+}
+
+static int rk3288_gpio_get_value(GpioOps *me)
+{
+	assert(me);
+	Rk3288Gpio *gpio = container_of(me, Rk3288Gpio, ops);
+
+	if (!gpio->dir_set) {
+		rk_gpio_set_pin_direction((int*)rk_gpio_bank_reg[gpio->bank], gpio->index, GPIO_IN);
+		gpio->dir_set = 1;
+	}
+	
+	return rk_gpio_get_pin_level((int*)rk_gpio_bank_reg[gpio->bank], gpio->index);
+}
+
+
+static int rk3288_gpio_set_value(GpioOps *me, unsigned value)
+{
+	assert(me);
+	Rk3288Gpio *gpio = container_of(me, Rk3288Gpio, ops);
+	rk_gpio_set_pin_level((int*)rk_gpio_bank_reg[gpio->bank], gpio->index, value);
+
+	if (!gpio->dir_set) {
+		rk_gpio_set_pin_direction((int*)rk_gpio_bank_reg[gpio->bank], gpio->index, GPIO_OUT);
+		gpio->dir_set = 1;
+	}
+
+	return 0;
+}
+
+Rk3288Gpio *new_rk3288_gpio(unsigned bank, unsigned index)
+{
+	die_if(bank >= GPIO_BANK_NUM ||
+	       index >= GPIO_INDEX_NUM,
+	       "GPIO parameters (%d, %d) out of bounds.\n",
+	       bank, index);
+
+	Rk3288Gpio *gpio = xzalloc(sizeof(*gpio));
+	gpio->bank = bank;
+	gpio->index = index;
+	return gpio;
+}
+
+Rk3288Gpio *new_rk3288_gpio_input(unsigned bank,	unsigned index)
+{
+	Rk3288Gpio *gpio = new_rk3288_gpio(bank, index);
+	gpio->ops.get = &rk3288_gpio_get_value;
+	return gpio;
+}
+
+Rk3288Gpio *new_rk3288_gpio_output(unsigned bank, unsigned index)
+{
+	Rk3288Gpio *gpio = new_rk3288_gpio(bank, index);
+	gpio->ops.set = &rk3288_gpio_set_value;
+	return gpio;
+}
---
+++ src/drivers/gpio/rk3288.h
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __RK3288_GPIO_H
+#define __RK3288_GPIO_H
+
+#include "drivers/gpio/gpio.h"
+
+/* gpio pin define */
+#define	GPIO_A0			0
+#define	GPIO_A1			1
+#define	GPIO_A2			2
+#define	GPIO_A3			3
+#define	GPIO_A4			4
+#define	GPIO_A5			5
+#define	GPIO_A6			6
+#define	GPIO_A7			7
+#define	GPIO_B0			8
+#define	GPIO_B1			9
+#define	GPIO_B2			10
+#define	GPIO_B3			11
+#define	GPIO_B4			12
+#define	GPIO_B5			13
+#define	GPIO_B6			14
+#define	GPIO_B7			15
+#define	GPIO_C0			16
+#define	GPIO_C1			17
+#define	GPIO_C2			18
+#define	GPIO_C3			19
+#define	GPIO_C4			20
+#define	GPIO_C5			21
+#define	GPIO_C6			22
+#define	GPIO_C7			23
+#define	GPIO_D0			24
+#define	GPIO_D1			25
+#define	GPIO_D2			26
+#define	GPIO_D3			27
+#define	GPIO_D4			28
+#define	GPIO_D5			29
+#define	GPIO_D6			30
+#define	GPIO_D7			31
+
+#define GPIO_BANK_NUM   9
+#define GPIO_INDEX_NUM	32
+#define MAX_GPIO_NUM		(GPIO_INDEX_NUM * GPIO_BANK_NUM)
+
+/* register list */
+#define GPIO_SWPORT_DR		0x00
+#define GPIO_SWPORT_DDR		0x04
+#define GPIO_INTEN		    0x30
+#define GPIO_INTMASK		0x34
+#define GPIO_INTTYPE_LEVEL	0x38
+#define GPIO_INT_POLARITY	0x3c
+#define GPIO_INT_STATUS		0x40
+#define GPIO_INT_RAWSTATUS	0x44
+#define GPIO_DEBOUNCE		0x48
+#define GPIO_PORTS_EOI		0x4c
+#define GPIO_EXT_PORT		0x50
+#define GPIO_LS_SYNC		0x60
+
+
+typedef struct Rk3288Gpio
+{
+	GpioOps ops;
+	int (*use)(struct Rk3288Gpio *me, unsigned use);
+	int dir_set;
+	unsigned bank;
+	unsigned index;
+} Rk3288Gpio;
+
+
+typedef enum eGPIOPinLevel {
+	GPIO_LOW=0,
+	GPIO_HIGH
+} eGPIOPinLevel_t;
+
+typedef enum eGPIOPinDirection {
+	GPIO_IN=0,
+	GPIO_OUT
+} eGPIOPinDirection_t;
+
+#endif	/* __RK3288_GPIO_H */
+
